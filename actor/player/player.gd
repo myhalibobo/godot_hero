2e806b0b3f8d8506c53a661f985c713c774ff400
bobo <276231002@qq.com>
2018-12-24 00:26:31 +0800

    方向旋转

--- actor/player/player.gd
+++ actor/player/player.gd
@@ -3,6 +3,7 @@ extends "../actor.gd"
 var hand:Sprite
 var cur_weapon
 var gun_direction_vec
+onready var camera = $camera
 
 func _ready():
 	hand = animations.get_animation_hand_node()
@@ -11,9 +12,13 @@ func _ready():
 	
 func _input(event):
 	if event is InputEventMouse:
-		var mouse_postion = event.global_position
-		var angle = mouse_postion.angle_to_point(hand.global_position)
+		var local_event = make_input_local(event)
+
+		var mouse_postion = local_event.position
+		var angle = mouse_postion.angle_to_point(hand.position)
 		hand.rotation = angle
+		print("mouse_postion:" , mouse_postion , "hand:",hand.position)
+		
 #		if angle > -PI/2 and angle < PI/2:
 #			scale.x = abs(scale.x) * 1 
 #		else:
